0205222adf38e5cc0bca4e23ce29b57ca6614d85
Rahul Singh Rajpurohit <143902285+Raghav8690@users.noreply.github.com>
2025-06-27 16:36:18 +0530

    Update sentiment_analysis.py
    add one more line of code to solve lookup error

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -14,6 +14,7 @@ import pickle
 nltk.download('stopwords', quiet=True)
 nltk.download('punkt', quiet=True)        
 nltk.download('wordnet', quiet=True)   
+nltk.download('punkt_tab', quiet=True)   
 nltk.download('omw-1.4', quiet=True) 
 
 stop_word = set(stopwords.words('english'))
